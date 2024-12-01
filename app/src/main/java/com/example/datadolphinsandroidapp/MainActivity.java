0f916109a4b740aee3c3f0164b63806a595382df
CS2426 <reecerram@Gmail.com>
2024-11-30 16:18:11 -0800

    Reece 003 beta screens

--- app/src/main/java/com/example/datadolphinsandroidapp/MainActivity.java
+++ app/src/main/java/com/example/datadolphinsandroidapp/MainActivity.java
@@ -10,7 +10,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
