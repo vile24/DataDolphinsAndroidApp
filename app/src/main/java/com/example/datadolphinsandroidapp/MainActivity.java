3be25571e57a2a5abb73e7af55c03841cabc89fe
Vy Le (Vivian) <vile@csumb.edu>
2024-12-12 09:37:46 -0800

    Restored main back to the original. Modified transactions page.

--- app/src/main/java/com/example/datadolphinsandroidapp/MainActivity.java
+++ app/src/main/java/com/example/datadolphinsandroidapp/MainActivity.java
@@ -9,31 +9,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.datadolphinsandroidapp.database.StockRepository;
-import com.example.datadolphinsandroidapp.database.StockWithQuantity;
 import com.example.datadolphinsandroidapp.databinding.ActivityMainBinding;
-
-import java.text.FieldPosition;
-import java.text.NumberFormat;
-import java.text.ParsePosition;
-//import com.example.datadolphinsandroidapp.databinding.ActivityPortfolioBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-
-    private StockRepository stockRepository;
-
-    //private ActivityPortfolioBinding binding;
-
-    private PortfolioAdapter adapter;
-
-    double fortfolioBalance = 0.0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,34 +23,9 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        stockRepository = StockRepository.getRepository(getApplication());
-
-        // Set up RecyclerView
-        adapter = new PortfolioAdapter(this::navigateToSellActivity);
-        binding.portfolioRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        binding.portfolioRecyclerView.setAdapter(adapter);
-
-        // Observe LiveData from Repository
-//        stockRepository.getStocksWithQuantities().observe(this, stockWithQuantities -> {
-//            adapter.submitList(stockWithQuantities);
-//        });
-
-        stockRepository.getStocksWithQuantities().observe(this, stockWithQuantities -> {
-                    Log.d("TransactionActivity", "Number of items: " + stockWithQuantities.size());
-                    for (StockWithQuantity stock : stockWithQuantities) {
-                        Log.d("TransactionActivity", "Ticker: " + stock.getTicker() + ", Quantity: " + stock.getQuantity());
-                        fortfolioBalance += stock.getPurchasePrice() * stock.getQuantity();
-                    }
-                    adapter.submitList(stockWithQuantities);
-                    NumberFormat formatter = NumberFormat.getCurrencyInstance();
-
-                    String formattedAmount = formatter.format(fortfolioBalance);
-
-            binding.portfolioBalancePlaceHolder.setText((formattedAmount));
-        });
 
         // Set up a button to navigate to BuyActivity
-        binding.buyStockButton.setOnClickListener(new View.OnClickListener() {
+        binding.buyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Open BuyActivity (no ticker passed from MainActivity)
@@ -77,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Set up a button to navigate to SellActivity ---no use
-//        binding.sellButton.setOnClickListener(new View.OnClickListener() {
+//        // Set up a button to navigate to BuyActivity
+//        binding.SellButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
 //                // Open SellActivity (no ticker passed from MainActivity)
@@ -87,22 +44,6 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
     }
-
-    private void navigateToSellActivity(StockWithQuantity stock) {
-        Intent intent = SellActivity.sellIntentFactory(this, stock.getTicker(), stock.getQuantity());
-        startActivity(intent);
-
-    }
-
-
-
-    // Bind Portfolio Button
-//        binding.btnPortfolio.setOnClickListener(v -> {
-//            // Navigate to TransactionActivity
-//            Intent intent = new Intent(MainActivity.this, TransactionActivity.class);
-//            startActivity(intent);
-//        });
-
 
     public static Intent openMain(Context context, String user){
         Intent intent = new Intent(context, MainActivity.class);
--- app/src/main/java/com/example/datadolphinsandroidapp/TransactionActivity.java
+++ app/src/main/java/com/example/datadolphinsandroidapp/TransactionActivity.java
@@ -16,7 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.datadolphinsandroidapp.database.StockRepository;
 import com.example.datadolphinsandroidapp.database.StockWithQuantity;
-import com.example.datadolphinsandroidapp.databinding.ActivityMainBinding;
+import com.example.datadolphinsandroidapp.databinding.ActivityTransactionBinding;
 
 import java.text.FieldPosition;
 import java.text.NumberFormat;
@@ -24,91 +24,5 @@ import java.text.ParsePosition;
 //import com.example.datadolphinsandroidapp.databinding.ActivityPortfolioBinding;
 
 public class TransactionActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-
-    private StockRepository stockRepository;
-
-    //private ActivityPortfolioBinding binding;
-
-    private PortfolioAdapter adapter;
-
-    double fortfolioBalance = 0.0;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        stockRepository = StockRepository.getRepository(getApplication());
-
-        // Set up RecyclerView
-        adapter = new PortfolioAdapter(this::navigateToSellActivity);
-        binding.portfolioRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        binding.portfolioRecyclerView.setAdapter(adapter);
-
-        // Observe LiveData from Repository
-//        stockRepository.getStocksWithQuantities().observe(this, stockWithQuantities -> {
-//            adapter.submitList(stockWithQuantities);
-//        });
-
-        stockRepository.getStocksWithQuantities().observe(this, stockWithQuantities -> {
-            Log.d("TransactionActivity", "Number of items: " + stockWithQuantities.size());
-            for (StockWithQuantity stock : stockWithQuantities) {
-                Log.d("TransactionActivity", "Ticker: " + stock.getTicker() + ", Quantity: " + stock.getQuantity());
-                fortfolioBalance += stock.getPurchasePrice() * stock.getQuantity();
-            }
-            adapter.submitList(stockWithQuantities);
-            NumberFormat formatter = NumberFormat.getCurrencyInstance();
-
-            String formattedAmount = formatter.format(fortfolioBalance);
-
-            binding.portfolioBalancePlaceHolder.setText((formattedAmount));
-        });
-
-        // Set up a button to navigate to BuyActivity
-        binding.buyStockButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Open BuyActivity (no ticker passed from TransactionActivity)
-                Intent intent = BuyActivity.buyIntentFactory(TransactionActivity.this);
-                startActivity(intent);
-            }
-        });
-
-        // Set up a button to navigate to SellActivity ---no use
-//        binding.sellButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Open SellActivity (no ticker passed from TransactionActivity)
-//                Intent intent = SellActivity.sellIntentFactory(TransactionActivity.this);
-//                startActivity(intent);
-//            }
-//        });
-    }
-
-    private void navigateToSellActivity(StockWithQuantity stock) {
-        Intent intent = SellActivity.sellIntentFactory(this, stock.getTicker(), stock.getQuantity());
-        startActivity(intent);
-
-    }
-
-
-
-    // Bind Portfolio Button
-//        binding.btnPortfolio.setOnClickListener(v -> {
-//            // Navigate to TransactionActivity
-//            Intent intent = new Intent(TransactionActivity.this, TransactionActivity.class);
-//            startActivity(intent);
-//        });
-
-
-    public static Intent openMain(Context context, String user){
-        Intent intent = new Intent(context, TransactionActivity.class);
-        intent.putExtra(USER, user);
-        //intent.getDataString();
-        return intent;
-    }
 
 }
